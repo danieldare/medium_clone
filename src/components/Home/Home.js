1b6350490276f52997ad8adfad979e988b3e28e9
Segun Oluwadare <dodanieloluwadare@gmail.com>
2019-09-06 12:58:14 +0100

    add trending component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Img1 from "../../editor3.jpg";
 import UserImg from "../../user.jpg";
+import GuitarImg from "../../guitar.jpg";
 import "./Home.scss";
 
 const Home = () => {
@@ -17,10 +18,13 @@ const Home = () => {
                </div> 
            </div>
 
-            <div className="">
                 <div className="container">
+            <div className="columns">
+                
                 <div className="column-2">
-                    <h3 className="heading__editor">Editor's Choice</h3>
+                    <h3 className="heading-editor"><span>Editor's Choice</span></h3>
+                    <div className="editor-section">
+
                     <div className="card">
                         <div className="card-content">
                             <img src={Img1} alt="img" width="100%" height="250" />
@@ -31,6 +35,41 @@ const Home = () => {
                             <div><img src={UserImg}  alt="img" className="author-img"/><p className="author__details">By <span className="text-purple">Segun Oluwadare </span> | 3 mins read</p></div>
                             <p className="date_mins"></p>
                         </div>
+                    </div>
+
+                    <div className="cards-container">
+                        <div className="card-small">
+                            <img src={GuitarImg}  alt="img" className="card-sm-img"/>
+                            <div className="card-sm-text">
+                                <h5 className="card-sm-title">The Night My Doorbell Camera Captured a Shooting</h5>
+                                <p className="genre">Lifestyle | 2019, 25 March</p>
+                                <p className="author__details">By <span className="text-purple">Segun Oluwadare </span> | 3 mins read</p>
+                            </div>
+                        </div>
+                        <div className="card-small">
+                            <img src={GuitarImg}  alt="img" className="card-sm-img"/>
+                            <div className="card-sm-text">
+                                <h5 className="card-sm-title">The Night My Doorbell Camera Captured a Shooting</h5>
+                                <p className="genre">Lifestyle | 2019, 25 March</p>
+                                <p className="author__details">By <span className="text-purple">Segun Oluwadare </span> | 3 mins read</p>
+                            </div>
+                        </div>
+
+                        <div className="card-small">
+                            <img src={GuitarImg}  alt="img" className="card-sm-img"/>
+                            <div className="card-sm-text">
+                                <h5 className="card-sm-title">The Night My Doorbell Camera Captured a Shooting</h5>
+                                <p className="genre">Lifestyle | 2019, 25 March</p>
+                                <p className="author__details">By <span className="text-purple">Segun Oluwadare </span> | 3 mins read</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="column-1">
+                    <h3 className="heading-trending"><span>Trending</span></h3>
+                    <div className="trending-container">
+                        hello
                     </div>
                 </div>
                 </div>
